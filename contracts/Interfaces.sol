4a8b3cc7851505cc98a12859156e8e8c6ec5409e
QuantSoldier <quantsoldier@gmail.com>
2020-10-25 18:12:42 -0700

    Add FE Interface

--- contracts/Interfaces.sol
+++ contracts/Interfaces.sol
@@ -24,6 +24,18 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
+interface IShitcoin {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function faucet() external; 
+}
+
 
 //UNISWAP
 interface IUniswapV2Factory {
